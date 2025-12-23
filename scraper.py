5532dbc9f00507e6e80021ccb040942fd64aa0b8
Damon Pashko <viper.ca082@outlook.com>
2025-12-23 14:16:37 -0700

    Started project

---
+++ scraper.py
@@ -0,0 +1,25 @@
+#!/bin/python
+
+import os
+import re
+import sys
+import requests
+from bs4 import BeautifulSoup
+from pathlib import Path
+from urllib.parse import urljoin, urlparse
+import time
+
+
+# class that contains data and methods for scraping
+class Scraper:
+	def __init__(self, albumURI, outputDIR):
+		self.albumURI = albumURI
+		self.outputDIR = Path(outputDIR)
+		self.session = requests.Session() #set up information for the 'web agent'
+		self.session.headers.update({ #define the scraper's user agent
+			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Gecko/20100101 Firefox/146.0'
+		})
+	
+	# removes the characters frome the file name that are not allowed
+	def cleanFileName(self, filename):
+		return re.sub(r'[<>:"/\\|?*]', '', filename)
